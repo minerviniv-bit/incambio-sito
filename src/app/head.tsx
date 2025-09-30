e9deb10d488765a5d7338d2192c90f22d3c5dc26
minerviniv-bit <minervini.v@gmail.com>
2025-09-30 21:22:28 +0200

    fix: rimosso preload duplicato immagine hero

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -1,12 +1,4 @@
 // src/app/head.tsx
 export default function Head() {
-  return (
-    <>
-      <link
-        rel="preload"
-        as="image"
-        href="/images/Settembre-2025.png"  // deve essere IDENTICO al src usato nell'hero
-      />
-    </>
-  );
+  return null;
 }
