54c6356df4877a4be92356115389f796f3c856fa
minerviniv-bit <minervini.v@gmail.com>
2025-09-30 10:45:09 +0200

    perf: preload hero image (migliora LCP/CLS mobile)

---
+++ src/app/head.tsx
@@ -0,0 +1,12 @@
+// src/app/head.tsx
+export default function Head() {
+  return (
+    <>
+      <link
+        rel="preload"
+        as="image"
+        href="/images/Settembre-2025.png"  // deve essere IDENTICO al src usato nell'hero
+      />
+    </>
+  );
+}
